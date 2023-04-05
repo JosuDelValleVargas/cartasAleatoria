c5600a2def133a3f3eb9472e38a60b752846283c
Josu DelValleVargas <josudelvallevargas@gmail.com>
2023-04-05 20:10:17 +0000

    entrega del proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,54 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const palos = ["♦", "♥", "♠", "♣"];
+  const numeros = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "rey",
+    "reina",
+    "jota"
+  ];
+
+  function cartaAleatorio(palos) {
+    return palos[Math.floor(Math.random() * palos.length)];
+  }
+  function cartaAleatorio(numeros) {
+    return numeros[Math.floor(Math.random() * numeros.length)];
+  }
+
+  const palo = cartaAleatorio(palos);
+  const numero = cartaAleatorio(numeros);
+
+  const carta = {
+    palo: palo,
+    numero: numero
+  };
+
+  console.log(carta);
+
+  let cardNumero = document.querySelector("#numero");
+  cardNumero.innerHTML = `<p>${carta.numero}</p>`;
+
+  if (carta.palo === "♦" || carta.palo === "♥") {
+    let cardSuperior = document.querySelector("#card_superior");
+    cardSuperior.innerHTML = `<p style="color: red;">${carta.palo}</p>`;
+
+    let cardInferior = document.querySelector("#card_inferior");
+    cardInferior.innerHTML = `<p style="color: red;">${carta.palo}</p>`;
+  } else {
+    let cardSuperior = document.querySelector("#card_superior");
+    cardSuperior.innerHTML = `<p>${carta.palo}</p>`;
+
+    let cardInferior = document.querySelector("#card_inferior");
+    cardInferior.innerHTML = `<p>${carta.palo}</p>`;
+  }
 };
